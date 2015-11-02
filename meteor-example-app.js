cbb6a199fd6e2b354c4d57bdf7d2a0664bea0632
Steven Miller <stevenmiller888@me.com>
2015-11-02 12:08:05 -0800

    added track event examples on client and server

--- meteor-example-app.js
+++ meteor-example-app.js
@@ -1,9 +1,12 @@
 analytics.load("x9a540mt9j");
 
 if (Meteor.isClient) {
-
+  analytics.track('Loaded on client!');
 }
 
 if (Meteor.isServer) {
-
+  analytics.track({
+    userId: 'foo',
+    event: 'Loaded on server!'
+  });
 }
